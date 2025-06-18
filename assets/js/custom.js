b11a712f0031f985e99b5c17b5f8639bb2ffcf4b
Al Amin <linkers4me@gmail.com>
2025-06-19 01:50:44 +0600

    added swiper pagination module

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const swipers = document.querySelectorAll('.mySwiper');
-    swipers.forEach(function (container) {
-        new Swiper(container, {
-            loop: true,
-            slidesPerView: 1,
-            grabCursor: true,
-            autoplay: {
-                delay: 4000,
-                disableOnInteraction: false,
-            },
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  const swipers = document.querySelectorAll(".mySwiper");
+  swipers.forEach(function (container) {
+    new Swiper(container, {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 10,
+      grabCursor: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
     });
+  });
 });
